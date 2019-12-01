4dfd345e08e7a71aa2e81619f1e29c631e0c5dc2
daniaalrababa9 <rababadania@gmail.com>
2019-12-01 03:22:33 +0200

    doing lab09

---
+++ schema.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE IF NOT EXISTS locations (
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  formatted_address VARCHAR(255),
+  latitude NUMERIC(10,7),
+  longitude NUMERIC(10,7)
+);
+
